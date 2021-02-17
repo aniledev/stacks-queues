ded9d2cda89a9c53800fafa1baa51348f435e1ff
aniledev <elinamcgill@gmail.com>
2021-02-17 12:11:10 -0500

    FEAT: write method for adding node to the top of a stack

--- node.js
+++ node.js
@@ -8,6 +8,23 @@ class _Node {
 
 // create the constructor for the stack with a empty first node
 class Stack {
-    constructor() {
-        this.top = null;
+  constructor() {
+    this.top = null;
+  }
+
+  push(data) {
+    /* If the stack is empty, then the node will be the pushed 
+    to top of the stack */
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
     }
+
+    /* If the stack already has something, 
+           then create a new node,
+           add data to the new node, and
+           have the pointer point to the top */
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+}
