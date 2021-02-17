15966730de099ccfa89abc8b512aa18f2c5e90a8
aniledev <elinamcgill@gmail.com>
2021-02-17 12:28:22 -0500

    FEAT: write enqueue method for adding to the end of a queue

--- queue.js
+++ queue.js
@@ -6,9 +6,29 @@ class _Node {
     }
 }
 
-// creates an empty queue 
+// creates an empty queue with values for the beginning and the end of the queue
 class Queue {
     constructor() {
         this.first = null;
         this.last = null;
+    
     }
+
+    enqueue (data) {
+        // create a new data node
+        const node = new _Node(data);
+
+        // if the queue is empty, the new node is the first node
+        if (this.first === null) {
+            this.first = node;
+        }
+
+        // if the queue is not empty, then the last node should point to the new node
+        if (this.last) {
+            this.last.next = node;
+        }
+        
+        //make the new node the last item on the queue
+        this.last = node;
+    }
+}
